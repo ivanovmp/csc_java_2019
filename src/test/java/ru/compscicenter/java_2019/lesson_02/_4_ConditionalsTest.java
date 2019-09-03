b9be2bc0d6ab5a721e3dc44fc6e6b095f0f912b6
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2019-09-03 11:59:39 +0300

    - добавил тестов под автобоксинг и свинч со строками

--- src/test/java/ru/compscicenter/java_2019/lesson_02/_4_ConditionalsTest.java
+++ src/test/java/ru/compscicenter/java_2019/lesson_02/_4_ConditionalsTest.java
@@ -187,6 +187,26 @@ public class _4_ConditionalsTest {
         assertEquals(falseCount.count, __);
     }
 
+    @Test
+    public void stringsInSwitchStatement() {
+        String[] animals = {"Dog", "Cat", "Tiger", "Elephant", "Zebra"};
+        String dangerous = null;
+        String notDangerous = null;
+        for (String animal : animals) {
+            switch (animal) {
+                case "Tiger":
+                    dangerous = animal;
+                case "Dog":
+                case "Cat":
+                case "Elephant":
+                case "Zebra":
+                    notDangerous = animal;
+            }
+        }
+        assertEquals(notDangerous, __);
+        assertEquals(dangerous, __);
+    }
+
     class Counter {
         boolean returnValue;
         int count = 0;
